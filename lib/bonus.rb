f1677d2f33b56f88241e92e474f1653db69278e3
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-09 21:27:18 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,8 +19,8 @@ def bonus
       ]
    }
   }
-[:montague][:hero][:status] = "dead"
-[:capulet][:heroine][:status] = "dead"
+[:montague][:hero][:status] = dead
+[:capulet][:heroine][:status] = dead
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
